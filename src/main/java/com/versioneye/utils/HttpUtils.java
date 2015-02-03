91502c7ebc0cbac671d9b5de413603b5745f7770
reiz <robert.reiz.81@gmail.com>
2015-02-03 21:36:16 +0100

    #35 Handle error codes from server!

--- src/main/java/com/versioneye/utils/HttpUtils.java
+++ src/main/java/com/versioneye/utils/HttpUtils.java
@@ -1,12 +1,16 @@
 package com.versioneye.utils;
 
+import com.versioneye.dto.ErrorJsonResponse;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
@@ -23,7 +27,52 @@ public class HttpUtils {
         multipartEntity.addPart(dataName, byteArrayBody);
         httpPost.setEntity( multipartEntity );
         HttpResponse response = client.execute(httpPost);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != 200){
+            String err = getErrorMessage(response);
+            String errMsg = "Status Code: " + statusCode + " -> " + err;
+            throw new Exception(errMsg);
+        }
+
         return new InputStreamReader(response.getEntity().getContent());
+    }
+
+    private static String getErrorMessage(HttpResponse response) throws Exception {
+        String errorMsg = getErrorFromJson(response);
+        if (errorMsg != null){
+            return errorMsg;
+        }
+        return getPureBodyString(response);
+    }
+
+    private static String getErrorFromJson(HttpResponse response){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorJsonResponse error = mapper.readValue(response.getEntity().getContent(), ErrorJsonResponse.class);
+            return error.getError();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String getPureBodyString(HttpResponse response){
+        try {
+            InputStream content = response.getEntity().getContent();
+            BufferedReader in = new BufferedReader(new InputStreamReader( content ) );
+            String inputLine;
+            StringBuffer body = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                body.append(inputLine);
+            }
+            in.close();
+            return body.toString();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return "";
+        }
+
     }
 
 }
